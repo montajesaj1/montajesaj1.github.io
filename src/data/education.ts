1d11fcb3a9fa85cb77598150ce9de0b0498bce3a
UBC Student <anthonyjosephmontajes@gmail.com>
2024-05-02 15:12:17 -0700

    Updated job changes

--- src/data/education.ts
+++ src/data/education.ts
@@ -3,8 +3,7 @@ export default [
     year: "Present",
     title: "University of British Columbia",
     duration: "Vancouver",
-    details:
-      "Major in Cognitive Systems, Minor in Computer Science | CGPA: 3.7",
+    details: "Major in Cognitive Systems, Minor in Computer Science",
     courses:
       "Courses: Data Structures and Algorithms I/II | Operating Systems I/II | Artificial Intelligence | Relational Databases | Applied Machine Learning",
   },
--- src/data/timeline.ts
+++ src/data/timeline.ts
@@ -1,10 +1,31 @@
 export default [
   {
-    year: "2017",
-    title: "Data Science Intern",
-    duration: "@ BlueSky Energy Technology",
+    year: "Current",
+    title: "Research Assistant",
+    duration: "@ Sauder School of Business",
     details:
-      "Promoted from Data Analyst intern to Data Scientist intern. Collaborated on LSTM neural networks and regression models for real-time energy predictions. Installed power-usage sensors and formatted training data.",
+      "Developing machine learning models investigating the relationship between market concentration and innovation pipelines in the pharmaceutical industry.",
+  },
+  {
+    year: "Current",
+    title: "Data Science Workshop Co-ordinator",
+    duration: "@ Visual Cognition Lab UBC",
+    details:
+      "Developing internal curriculums to address lab-specific painpoints in data analytics. Collaborated with developers and project managers to identify potential areas of improvement.",
+  },
+  {
+    year: "2023",
+    title: "Operations Assistant",
+    duration: "@ CEDaR Space CIS UBC",
+    details:
+      "Coordinated a map-based exhibit for UBC's MOA. Implemented front-ends using ReactJS, NodeJS, and Chakra UI. Engaged with IT teams for future web to native app transition.",
+  },
+  {
+    year: "2023",
+    title: "Data Science CoPilot",
+    duration: "@ Visual Cognition Lab UBC",
+    details:
+      "Worked on the 'Correlation Project'. Built experiment conditions with JavaScript, NodeJS, and Firebase. Streamlined data analysis with Python.",
   },
   {
     year: "2020",
@@ -14,17 +35,10 @@ export default [
       "First application deployment on OpenShift 4. Maintained Confluence documentation and presented deployment methods. Developed a Flask and SQL based 'Ticker Search' web app.",
   },
   {
-    year: "Present",
-    title: "Operations Engineer",
-    duration: "@ CEDaR Space CIS UBC",
+    year: "2017",
+    title: "Data Science Intern",
+    duration: "@ BlueSky Energy Technology",
     details:
-      "Coordinated a map-based exhibit for UBC's MOA. Implemented front-ends using ReactJS, NodeJS, and Chakra UI. Engaged with IT teams for future web to native app transition.",
-  },
-  {
-    year: "Present",
-    title: "Data Science CoPilot",
-    duration: "@ Visual Cognition Lab UBC",
-    details:
-      "Worked on the 'Correlation Project'. Built experiment conditions with JavaScript, NodeJS, and Firebase. Streamlined data analysis with Python.",
+      "Promoted from Data Analyst intern to Data Scientist intern. Collaborated on LSTM neural networks and regression models for real-time energy predictions. Installed power-usage sensors and formatted training data.",
   },
 ];
